503b19138a5b259b5a3cb68658b40a23b1ff06e3
nilayganatra027@gmail.com <nilayganatra027@gmail.com>
2024-11-30 09:19:00 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to node azure cicd");
+});
+app.get("/about",(req,res)=>{
+    res.json({
+        message:"This simple node app",
+        developer:"Nilay Ganatra",
+    })
+});
+app.listen(PORT,'0.0.0.0',()=>{
+    console.log(`server is running on ${PORT}`);
+})
